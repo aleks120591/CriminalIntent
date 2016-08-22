7ece298f08b1d5b7c46c8e994545c91376b3caee
aleks120591@gmail.com <0p8i6y4r2w>
2016-08-22 13:05:58 +0400

    Date picker

--- app/src/main/java/com/bignerdranch/android/criminalintent/fragment/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/fragment/CrimeFragment.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.criminalintent.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -24,6 +25,7 @@ import java.util.UUID;
 public class CrimeFragment extends Fragment {
 
     private static final String ARG_CRIME_ID = "crime_id";
+    private static final String DIALOG_DATE = "DialogDate";
 
     private Crime mCrime;
     private EditText mTitleField;
@@ -63,7 +65,15 @@ public class CrimeFragment extends Fragment {
         });
         mDateButton = (Button) v.findViewById(R.id.crime_date);
         mDateButton.setText(mCrime.getDate().toString());
-        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager manager = getFragmentManager();
+                DatePickerFragment dialog = new DatePickerFragment();
+                dialog.show(manager, DIALOG_DATE);
+
+            }
+        });
 
         mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
--- app/src/main/java/com/bignerdranch/android/criminalintent/fragment/DatePickerFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/fragment/DatePickerFragment.java
@@ -1,8 +1,29 @@
 package com.bignerdranch.android.criminalintent.fragment;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.bignerdranch.android.criminalintent.R;
+
 /**
  * Created by Warlock on 22.08.2016.
  */
 
-public class DatePickerFragment {
+public class DatePickerFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date, null);
+
+        return new AlertDialog.Builder(getActivity())
+                .setView(v)
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
 }
