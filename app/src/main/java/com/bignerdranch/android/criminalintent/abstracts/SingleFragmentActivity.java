18f3ac7f8d65383baeb94a2bbc62973fba5497a3
aleks120591@gmail.com <0p8i6y4r2w>
2016-08-22 16:59:28 +0400

    theme, style

--- app/src/main/java/com/bignerdranch/android/criminalintent/abstracts/SingleFragmentActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/abstracts/SingleFragmentActivity.java
@@ -2,8 +2,8 @@ package com.bignerdranch.android.criminalintent.abstracts;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 import com.bignerdranch.android.criminalintent.R;
 
@@ -11,7 +11,7 @@ import com.bignerdranch.android.criminalintent.R;
  * Created by Warlock Aleks on 20.08.2016.
  */
 
-public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     protected abstract Fragment createFragment();
 
