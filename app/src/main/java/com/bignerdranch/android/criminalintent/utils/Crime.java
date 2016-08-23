f87c82a9c348dc007f2b290617df01dc605c1333
aleks120591@gmail.com <0p8i6y4r2w>
2016-08-23 16:03:31 +0400

    database

--- app/src/main/java/com/bignerdranch/android/criminalintent/utils/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/utils/Crime.java
@@ -12,6 +12,7 @@ public class Crime {
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
+    private String mSuspect;
 
     public Crime() {
         this(UUID.randomUUID());
@@ -20,6 +21,14 @@ public class Crime {
     public Crime(UUID id) {
         mId = id;
         mDate = new Date();
+    }
+
+    public String getSuspect() {
+        return mSuspect;
+    }
+
+    public void setSuspect(String suspect) {
+        mSuspect = suspect;
     }
 
     public UUID getId() {
