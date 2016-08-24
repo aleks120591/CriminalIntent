eb996e19c7262a0c429f42bc4c20056e2e65ecd2
aleks120591@gmail.com <0p8i6y4r2w>
2016-08-24 15:22:50 +0400

    Begin

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,8 +1,17 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
 /**
  * Created by Warlock on 24.08.2016.
  */
 
-public class CrimeActivity {
+public class CrimeActivity extends FragmentActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+    }
 }
