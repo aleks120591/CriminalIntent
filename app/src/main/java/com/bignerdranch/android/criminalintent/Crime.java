9faf250246e905a3f14dd60f64571d51037f592e
aleks120591@gmail.com <0p8i6y4r2w>
2016-08-24 14:59:49 +0400

    Begin

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,49 +1,8 @@
 package com.bignerdranch.android.criminalintent;
 
-import java.util.Date;
-import java.util.UUID;
+/**
+ * Created by Warlock on 24.08.2016.
+ */
 
 public class Crime {
-
-    private UUID mId;
-    private String mTitle;
-    private Date mDate;
-    private boolean mSolved;
-
-    public Crime() {
-        mId = UUID.randomUUID();
-        mDate = new Date();
-    }
-
-    public UUID getId() {
-        return mId;
-    }
-
-    public void setId(UUID id) {
-        mId = id;
-    }
-
-    public String getTitle() {
-        return mTitle;
-    }
-
-    public void setTitle(String title) {
-        mTitle = title;
-    }
-
-    public boolean isSolved() {
-        return mSolved;
-    }
-
-    public void setSolved(boolean solved) {
-        mSolved = solved;
-    }
-
-    public Date getDate() {
-        return mDate;
-    }
-
-    public void setDate(Date date) {
-        mDate = date;
-    }
 }
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,24 +1,8 @@
 package com.bignerdranch.android.criminalintent;
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
+/**
+ * Created by Warlock on 24.08.2016.
+ */
 
-public class CrimeActivity extends FragmentActivity{
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_conainer);
-        if (fragment == null) {
-            fragment = new CrimeFragment();
-            fm.beginTransaction()
-                    .add(R.id.fragment_conainer, fragment)
-                    .commit();
-        }
-    }
+public class CrimeActivity {
 }
