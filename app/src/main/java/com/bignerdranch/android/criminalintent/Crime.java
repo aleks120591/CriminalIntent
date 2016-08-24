b8f8ab8fb2fd1fade8d438a7e9e65c5503e72e0e
aleks120591@gmail.com <0p8i6y4r2w>
2016-08-24 15:35:12 +0400

    Begin

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,8 +1,29 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.UUID;
+
 /**
  * Created by Warlock on 24.08.2016.
  */
 
 public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime(){
+        mId=UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
 }
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -1,8 +1,19 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
 /**
  * Created by Warlock on 24.08.2016.
  */
 
-public class CrimeFragment {
+public class CrimeFragment extends Fragment {
+
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
 }
