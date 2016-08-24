46be7ee4cfc0508e2758f95646064f02758f04e6
aleks120591@gmail.com <0p8i6y4r2w>
2016-08-24 12:44:00 +0400

    Begin

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -3,10 +3,6 @@ package com.bignerdranch.android.criminalintent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
-/**
- * Created by Warlock on 24.08.2016.
- */
-
 public class CrimeActivity extends FragmentActivity{
 
     @Override
