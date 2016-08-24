21f9d0c47eb5bc0ecab49e9cbff9012c5daa70a6
aleks120591@gmail.com <0p8i6y4r2w>
2016-08-24 11:59:40 +0400

    Begin

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -12,5 +12,6 @@ public class CrimeActivity extends FragmentActivity{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
     }
 }
