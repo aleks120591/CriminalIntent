7fc5e8c349eae02d03be3f643b641ab658b98a75
aleks120591@gmail.com <0p8i6y4r2w>
2016-08-25 14:20:45 +0400

    Begin

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -38,10 +38,10 @@ public class CrimePagerActivity extends FragmentActivity {
             }
         });
 
-        UUID crimeId=(UUID)getIntent()
+        UUID crimeId = (UUID) getIntent()
                 .getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
-        for (int i=0;i<mCrimes.size();i++){
-            if (mCrimes.get(i).getId().equals(crimeId)){
+        for (int i = 0; i < mCrimes.size(); i++) {
+            if (mCrimes.get(i).getId().equals(crimeId)) {
                 mViewPager.setCurrentItem(i);
                 break;
             }
