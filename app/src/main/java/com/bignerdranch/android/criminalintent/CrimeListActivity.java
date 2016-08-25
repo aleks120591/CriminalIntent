73be850cbfe8b13a3b34f08d18de259d2d5916db
aleks120591@gmail.com <0p8i6y4r2w>
2016-08-25 14:46:15 +0400

    Begin

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -1,11 +1,5 @@
 package com.bignerdranch.android.criminalintent;
 
-import android.support.v4.app.Fragment;
+public class CrimeListActivity {
 
-public class CrimeListActivity extends SingleFragmentActivity {
-
-    @Override
-    protected Fragment createFragment() {
-        return new CrimeListFragment();
-    }
 }
